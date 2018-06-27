fb284ae43e532a1fb7ff96882c4623c667618648
duy.ly <duy.ly@ntq-solution.com.vn>
2018-06-27 19:29:24 +0900

    v4.0.0

--- renderer.js
+++ renderer.js
@@ -3,111 +3,132 @@
 // All of the Node.js APIs are available in this process.
 
 const { clipboard } = require('electron')
+var tinycolor = require("tinycolor2")
 
 var screens = [
     {
+        id: '37201',
         full: '37201 従業員選択 (Select employee)',
         japanese: '37201従業員選択',
         english: '37201 従業員選択 (Select employee)',
     },
     {
+        id: '36801',
         full: '36801 商事商品選択 (Select trader and goods)',
         japanese: '36801商事商品選択',
         english: '36801 商事商品選択 (Select trader and goods)',
     },
     {
+        id: '36901',
         full: '36901 事業所選択 (Select Division)',
         japanese: '36901事業所選択',
         english: '36901 事業所選択 (Select Division)',
     },
     {
+        id: '37001',
         full: '37001 保管場所選択 (select Storage)',
         japanese: '37001保管場所選択',
         english: '37001 保管場所選択 (select Storage)',
     },
     {
+        id: '36701',
         full: '36701 グレード選択 (Select grade)',
         japanese: '36701グレード選択',
         english: '36701 グレード選択 (Select grade)',
     },
     {
+        id: '37501',
         full: '37501 依頼作業No選択 (Select Request work No)',
         japanese: '37501依頼作業No選択',
         english: '37501 依頼作業No選択 (Select Request work No)',
     },
     {
+        id: '37101',
         full: '37101 顧客選択 (Select customer)',
         japanese: '37101顧客選択',
         english: '37101 顧客選択 (Select customer)',
     },
     {
+        id: '37301',
         full: '37301 営業注番選択 (Select sale order no)',
         japanese: '37301営業注番選択',
         english: '37301 営業注番選択 (Select sale order no)',
     },
     {
+        id: '38301',
         full: '38301 業種選択 (Select industry)',
         japanese: '38301業種選択',
         english: '38301 業種選択 (Select industry)',
     },
     {
+        id: '14201',
         full: '14201 自決登録(販売条件) Self registration (Sale condition)',
         japanese: '14201自決登録(販売条件)',
         english: '14201 自決登録(販売条件) Self registration (Sale condition)',
     },
     {
+        id: '38101',
         full: '38101 中本見積No選択 (Select used machine)',
         japanese: '38101中本見積No選択',
         english: '38101 中本見積No選択 (Select used machine)',
     },
     {
+        id: '37401',
         full: '37401 在庫No選択 (Select inventory No)',
         japanese: '37401在庫No選択',
         english: '37401 在庫No選択 (Select inventory No)',
     },
     {
+        id: '14901',
         full: '14901 自決登録（間接輸出）Self registration (Indirect export)',
         japanese: '14901自決登録（間接輸出）',
         english: '14901 自決登録（間接輸出）Self registration (Indirect export)',
     },
     {
+        id: '14601',
         full: '14601 自決登録(競合) Self registration (Competition)',
         japanese: '14601自決登録(競合)',
         english: '14601 自決登録(競合) Self registration (Competition)',
     },
     {
+        id: '13101',
         full: '13101 自決登録（集金）Self registration (Bill collect)',
         japanese: '13101自決登録（集金）',
         english: '13101 自決登録（集金）Self registration (Bill collect)',
     },
     {
+        id: '13301',
         full: '13301 自決登録（コメント）Self registration (comment)',
         japanese: '13301自決登録（コメント）',
         english: '13301 自決登録（コメント）Self registration (comment)',
     },
     {
+        id: '15901',
         full: '15901 送り先選択 (select address)',
         japanese: '15901送り先選択',
         english: '15901 送り先選択 (select address)',
     },
     {
+        id: '14501',
         full: '14501 自決登録(配送情報) Self registration (Delivery info)',
         japanese: '14501自決登録(配送情報)',
         english: '14501 自決登録(配送情報) Self registration (Delivery info)',
     },
     {
+        id: '14301',
         full: '14301 自決登録(付帯費用) (Self registration (Additional expenses)',
         japanese: '14301自決登録(付帯費用)',
         english: '14301 自決登録(付帯費用) (Self registration (Additional expenses)',
     },
     {
+        id: '13701',
         full: '13701 自決登録(下取) (Self registration (Trade in))',
         japanese: '13701自決登録(下取)',
         english: '13701 自決登録(下取) (Self registration (Trade in))',
     },
 ];
 
-var filteredScreens = [];
+var filteredScreens = screens;
 
 var trans = {
     'from_to': '— %s-%s%',
@@ -138,7 +159,14 @@ function getData() {
 function display(data) {
     var html = '<p><strong>' + data.length + ' found</strong></p>';
     data.forEach(function(value) {
-        html += '<div style="background:#fefefe;line-height:1;margin:12px 0;padding: 2px 3px 4px 3px;" data-copy="' + encodeURIComponent(JSON.stringify(value)) +'">' +
+        var color = '#fefefe';
+        if (!mode) {
+            var color = normalBackgrounds[Math.floor(Math.random()*normalBackgrounds.length)];
+        }
+
+        var border = tinycolor(color).darken().toString();
+
+        html += '<div class="screenDiv" style="background:'+color+';border: 1px solid '+border+';line-height:1;margin:12px 0;padding: 2px 3px 4px 3px;" data-copy="' + encodeURIComponent(JSON.stringify(value)) +'">' +
         'JSTSAS' + value.full +
         '<div style="float:right"><button class="copyJapaneseBtn" style="border: none;background: #ccc;">COPY JAPANESE</button>' +
         ' <button class="copyBtn" style="border: none;background: #ccc;">COPY FULL</button></div></div>';
@@ -193,17 +221,68 @@ document.addEventListener('click', function (e) {
 
         new Notification('Đã copy');
     }
+
+    if (e.target.classList.contains('screenDiv')) {
+        var data = JSON.parse(decodeURIComponent(e.target.dataset.copy));
+        document.querySelector('#btnEd').value = data.id;
+        getData.call(document.querySelector('#btnEd'));
+    }
+});
+
+var mode = "";
+
+var normalBackgrounds = [
+    '#FFCDD2',
+    '#F8BBD0',
+    '#E1BEE7',
+    '#C5CAE9',
+    '#BBDEFB',
+    '#B2EBF2',
+    '#B2DFDB',
+    '#A5D6A7',
+    '#C5E1A5',
+    '#FFF9C4',
+    '#FFECB3',
+    '#FFE0B2',
+    '#FFCCBC',
+    '#D7CCC8',
+    '#E0E0E0',
+    '#CFD8DC',
+    '#CFD8DC',
+    '#81C784',
+    '#69F0AE',
+    '#FFD180',
+    '#FFAB40'
+];
+
+var hentaiBackgrounds = [...Array(68).keys()].map(function(index) {
+    return 'images/hentai/' + index.toString().padStart(4, '0') + '.jpg';
 });
 
 function changeBackground() {
-    var items = [
-        'images/Seppuku.jpg',
-        'images/bg-tokai.JPEG'
-    ];
-    var image = items[Math.floor(Math.random()*items.length)];;
-    document.getElementsByTagName('body')[0].style.backgroundImage = `url(${image})`;
-}
+    switch (mode) {
+        case 'hentai':
+            var items = hentaiBackgrounds;
+            break;
+        default:
+            var items = normalBackgrounds;
+    }
 
+    var image = items[Math.floor(Math.random()*items.length)];
+    if (image.startsWith('#')) {
+        document.getElementsByTagName('body')[0].style.backgroundImage = ``;
+    } else {
+        document.getElementsByTagName('body')[0].style.backgroundImage = `url(${image})`;
+    }
+    }
+
+document.querySelector('#modeSelect').addEventListener('change', function() {
+    mode = this.value;
+    changeBackground();
+    display(filteredScreens);
+})
+
+changeBackground();
 setInterval(changeBackground, 6000);
 
-display(screens);
+display(filteredScreens);
